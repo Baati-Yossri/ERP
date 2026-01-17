f51ba5b2a9535a6ae5fe04c36c311b78adf1ba22
Baati Yosri <baatiyossri@gmaill.com>
2026-01-17 18:30:15 +0100

    first one

---
+++ Month-01-Backend-Basics/erp-backend/main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+import psycopg2
+
+app = FastAPI()
+
+# Connect to the School database
+conn = psycopg2.connect(
+    dbname="ERP",
+    user="postgres",
+    password="123456789",
+    host="localhost",
+    port="5432"
+)
+
+@app.get("/")
+def home():
+    return {"message": "ERP backend running"}
+
+@app.get("/users")
+def recuperer_users():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    cursor.close()
+    return users
+
+@app.post("/users")
+def ajouter_user(user: dict):
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (user['name'], user['email']))
+    conn.commit()
+    cursor.close()
+    return {"message": "User added successfully"}
+@app.get("/users/{id}")
+def recuperer_user(id:int):
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id=%s",(id,))
+    user=cursor.fetchone()
+    if user:
+        cursor.close()
+        return user
+    else:
+        cursor.close()
+        return {"message": "user not found"}
+@app.put("/users/{id}")
+def modifier_user(id:int,user:dict):
+    cursor=conn.cursor()
+    cursor.execute("UPDATE users SET name=%s, email=%s WHERE id=%s",(user["name"],user["email"],id))
+    conn.commit()
+    cursor.close()
+    return {"message": "user updated successfully"}
+@app.delete("/users/{id}")
+def supprimer_user(id:int):
+    cursor=conn.cursor()
+    cursor.execute("DELETE FROM users WHERE id=%s",(id,))
+    conn.commit()
+    cursor.close()
+    return {"message": "user deleted successfully"}
+  
---
+++ Month-01-Backend-Basics/readme.md
@@ -0,0 +1,14 @@
+# Month 01 – Backend Basics
+
+This month focuses on backend fundamentals.
+
+## Implemented features
+- REST API with FastAPI
+- PostgreSQL connection
+- CRUD operations for users
+- Swagger documentation
+
+## How to run
+```bash
+pip install -r requirements.txt
+uvicorn main:app --reload
